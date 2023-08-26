e4831d273a6c8ba25e4eb4a0daf875cfcaf508cf
Zachary Ringwood <zringwood@gmail.com>
2023-08-26 18:49:51 -0400

    Fixed the chevrons

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -7,7 +7,7 @@ function Skill({title, writeup}){
         <div className="skill">
         <h4 className="skill__title">{title}</h4>
         {writeup.length > 0 && <button className={`skill__chevron ${displayWriteup && "skill__chevron--clicked"}`}  alt="" onClick={() => setDisplayWriteup(!displayWriteup)}/>}
-        {displayWriteup && <div className="skill__dropdown">{writeup}</div>}
+        {displayWriteup && <p className="skill__dropdown">{writeup}</p>}
         </div>
     )
 }
