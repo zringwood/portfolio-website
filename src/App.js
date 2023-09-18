b94d74735d9046e97ebdc957334797b3d3575ffe
Zachary Ringwood <zringwood@gmail.com>
2023-09-17 21:09:20 -0400

    Implemented theme

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import Biography from "./pages/Biography/Biography";
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Tactics from "./pages/Tactics/Tactics";
 import NavigationMenu from "./components/NavigationMenu/NavigationMenu";
+import { ThemeProvider, createTheme } from "@mui/material";
 function App() {
+  const theme = createTheme()
   return (
+    <ThemeProvider theme={theme}>
     <BrowserRouter>
     <NavigationMenu />
     <Routes>
@@ -11,6 +14,7 @@ function App() {
       <Route path="/tactics" element={<Tactics projectURL={"https://tactics.zacharyringwood.com"}/>}/>
     </Routes>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
