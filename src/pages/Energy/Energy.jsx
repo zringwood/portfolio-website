6f3c992d472b00142d9f111e7ab22d573f541385
Zachary Ringwood <zringwood@gmail.com>
2023-09-23 14:11:12 -0400

    working on energy writeup

--- src/pages/Energy/Energy.jsx
+++ src/pages/Energy/Energy.jsx
@@ -11,7 +11,11 @@ export default function Energy(){
             justifyContent: "center",
             alignItems: "center",
             backgroundColor:"background.default"}}>
-        <Typography component="h2" variant="h2">Coming Soon!</Typography>
+        <Typography component="h1" variant="h2">Energy</Typography>
+        <Typography variant="body1">Much of the details of this project are still under NDA, so I can't discuss the client's name or any of the data involved. However, I can discuss what I built.</Typography>
+        <Typography component="h2" variant="h4">MySQL from Scratch</Typography>
+        <Typography variant="body1">The client </Typography>
+
         </Box>
     )
 }
